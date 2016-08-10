04c279f37f0a34d9c4cfc3ca39723353bd01cbbb
galhajaj <galhajaj@gmail.com>
2016-08-11 00:23:02 +0300

    start the frame

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainManager : MonoBehaviour 
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("manageArmyScene");
+    }
+}
