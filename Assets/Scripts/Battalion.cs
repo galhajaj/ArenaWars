2c18bcdd4f3a6bd646b6e27cd068322b3127592f
galhajaj <galhajaj@gmail.com>
2016-08-12 18:43:49 +0300

    add circles with params + fray object and battle

--- Assets/Scripts/Battalion.cs
+++ Assets/Scripts/Battalion.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 
 public enum ForceType
 {
-    Infantry,
-    Knights,
-    Archers
+    INFANTRY,
+    KNIGHTS,
+    ARCHERS,
+    PIKES,
+    NONE
 }
 
 public enum ForceAligment
@@ -22,9 +24,13 @@ public class Battalion : MonoBehaviour
     public ForceAligment Aligment;
     public int NumberOfUnits;
     public float MoveSpeed;
+    public bool IsCollided = false;
+    public GameObject FrayObject;
 
     private bool _isHandled = false;
     private List<Vector3> _waypoints = new List<Vector3>();
+
+    readonly Color PURPLE_COLOR = new Color(1.0F, 0.0F, 1.0F);
 
     // =============================================================================================================== //
 	void Start () 
@@ -35,6 +41,7 @@ public class Battalion : MonoBehaviour
 	void Update () 
     {
         updateSize();
+        updateColor();
 
         // if is handled collect waypoints from mouse position
         if (_isHandled)
@@ -63,20 +70,58 @@ public class Battalion : MonoBehaviour
     // =============================================================================================================== //
     void OnMouseDown() 
     {
+        if (Aligment != ForceAligment.FRIENDLY)
+            return;
         _waypoints.Clear();
         _isHandled = true;
+    }
+    // =============================================================================================================== //
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<Fray>() != null)
+            return;
+
+        if (other.gameObject.GetComponent<Battalion>().Aligment == Aligment)
+            return;
+
+        if (IsCollided)
+            return;
+
+        GameObject fray = Instantiate(FrayObject, other.transform.position, Quaternion.identity) as GameObject;
+        fray.GetComponent<Fray>().Init(this.gameObject, other.gameObject);
+        int mySign = (Aligment == ForceAligment.FRIENDLY) ? 1 : -1;
+        int otherSign = (other.gameObject.GetComponent<Battalion>().Aligment == ForceAligment.FRIENDLY) ? 1 : -1;
+        transform.position = new Vector3(mySign * 10000.0F, mySign * 10000.0F);
+        other.transform.position = new Vector3(otherSign * 10000.0F, otherSign * 10000.0F);
+        /*NumberOfUnits += other.gameObject.GetComponent<Battalion>().NumberOfUnits;
+        Aligment = ForceAligment.NEUTRAL;
+        _waypoints.Clear();
+        _isHandled = false;*/
+
+        other.gameObject.GetComponent<Battalion>().IsCollided = true;
+        /*Destroy(other.gameObject);*/
     }
     // =============================================================================================================== //
     private void updateSize()
     {
         // number of units is between 250 to 1000
         int units = NumberOfUnits;
-        if (units < 250)
-            units = 250;
-        if (units > 1000)
-            units = 1000;
+        if (units < 100)
+            units = 100;
+        /*if (units > 1000)
+            units = 1000;*/
         float scale = units / 1000.0F;
         transform.localScale = new Vector3(scale, scale, scale);
+    }
+    // =============================================================================================================== //
+    private void updateColor()
+    {
+        if (Aligment == ForceAligment.FRIENDLY)
+            this.GetComponent<SpriteRenderer>().color = Color.blue;
+        else if (Aligment == ForceAligment.OPPOSING)
+            this.GetComponent<SpriteRenderer>().color = Color.red;
+        else
+            this.GetComponent<SpriteRenderer>().color = PURPLE_COLOR;
     }
     // =============================================================================================================== //
 }
---
+++ Assets/Scripts/Fray.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fray : MonoBehaviour 
+{
+    private int _numberOfUnits;
+    private GameObject _rival1;
+    private GameObject _rival2;
+    private Battalion _rival1Script;
+    private Battalion _rival2Script;
+    // =============================================================================================================== //
+	void Start () 
+    {
+	
+	}
+    // =============================================================================================================== //
+	void Update () 
+    {
+        updateBattle();
+        updateSize();
+	}
+    // =============================================================================================================== //
+    public void Init(GameObject rival1, GameObject rival2)
+    {
+        _rival1 = rival1;
+        _rival2 = rival2;
+        _rival1Script = _rival1.GetComponent<Battalion>();
+        _rival2Script = _rival2.GetComponent<Battalion>();
+    }
+    // =============================================================================================================== //
+    private void updateSize()
+    {
+        _numberOfUnits = _rival1Script.NumberOfUnits + _rival2Script.NumberOfUnits;
+
+        // number of units is between 250 to 1000
+        int units = _numberOfUnits;
+        if (units < 100)
+            units = 100;
+        /*if (units > 1000)
+            units = 1000;*/
+        float scale = units / 1000.0F;
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+    // =============================================================================================================== //
+    private void updateBattle()
+    {
+        int rand = Random.Range(0, 3);
+        if (rand == 0)
+            _rival1Script.NumberOfUnits--;
+        else if (rand == 1)
+            _rival2Script.NumberOfUnits--;
+
+        if (_rival1Script.NumberOfUnits <= 0)
+        {
+            _rival2.transform.position = this.transform.position;
+            _rival2Script.IsCollided = false;
+            Destroy(this.gameObject);
+        }
+
+        if (_rival2Script.NumberOfUnits <= 0)
+        {
+            _rival1.transform.position = this.transform.position;
+            _rival1Script.IsCollided = false;
+            Destroy(this.gameObject);
+        }
+    }
+    // =============================================================================================================== //
+}
