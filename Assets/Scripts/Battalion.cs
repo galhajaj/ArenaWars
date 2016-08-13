af831f21618ee389834583de564ec84f7e89fec0
galhajaj <galhajaj@gmail.com>
2016-08-13 23:50:57 +0300

    add not-accurate managing of battalions

--- Assets/Scripts/Battalion.cs
+++ Assets/Scripts/Battalion.cs
@@ -27,8 +27,12 @@ public class Battalion : MonoBehaviour
     public bool IsCollided = false;
     public GameObject FrayObject;
 
-    private bool _isHandled = false;
     private List<Vector3> _waypoints = new List<Vector3>();
+    public List<Vector3> Waypoints
+    {
+        get { return _waypoints; }
+        set { _waypoints = value; }
+    }
 
     readonly Color PURPLE_COLOR = new Color(1.0F, 0.0F, 1.0F);
 
@@ -43,38 +47,16 @@ public class Battalion : MonoBehaviour
         updateSize();
         updateColor();
 
-        // if is handled collect waypoints from mouse position
-        if (_isHandled)
-        {
-            Vector3 waypoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
-            _waypoints.Add(waypoint);
-        }
-
-        // mouse release
-        if (Input.GetMouseButtonUp(0))
-        {
-            _isHandled = false;
-        }
-
 	    // move towards stored waypoints and remove them while arrived
         if (_waypoints.Count > 0)
         {
-            var targetPos = Camera.main.ScreenToWorldPoint(_waypoints[0]);
-            targetPos.z = transform.position.z;
+            Vector3 targetPos = _waypoints[0];
             transform.position = Vector3.MoveTowards(transform.position, targetPos, MoveSpeed * Time.deltaTime);
             // if close to waypoint remove it
             if (Vector3.Distance(targetPos, transform.position) <= 0.01F)
                 _waypoints.RemoveAt(0);
         }
 	}
-    // =============================================================================================================== //
-    void OnMouseDown() 
-    {
-        if (Aligment != ForceAligment.FRIENDLY)
-            return;
-        _waypoints.Clear();
-        _isHandled = true;
-    }
     // =============================================================================================================== //
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -93,13 +75,8 @@ public class Battalion : MonoBehaviour
         int otherSign = (other.gameObject.GetComponent<Battalion>().Aligment == ForceAligment.FRIENDLY) ? 1 : -1;
         transform.position = new Vector3(mySign * 10000.0F, mySign * 10000.0F);
         other.transform.position = new Vector3(otherSign * 10000.0F, otherSign * 10000.0F);
-        /*NumberOfUnits += other.gameObject.GetComponent<Battalion>().NumberOfUnits;
-        Aligment = ForceAligment.NEUTRAL;
-        _waypoints.Clear();
-        _isHandled = false;*/
 
         other.gameObject.GetComponent<Battalion>().IsCollided = true;
-        /*Destroy(other.gameObject);*/
     }
     // =============================================================================================================== //
     private void updateSize()
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour 
+{
+    private GameObject _handledObject = null;
+    private Vector3 _deltaOfClickFromObject;
+
+    readonly float MIN_DISTANCE_TO_MANAGE_OBJECT = 1.0F;
+    // =============================================================================================================== //
+	void Start () 
+    {
+	
+	}
+    // =============================================================================================================== //
+	void Update () 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _handledObject = findClosestObjectToMousePosition();
+            if (_handledObject != null)
+            {
+                _handledObject.GetComponent<Battalion>().Waypoints.Clear();
+            }
+        }
+
+        // if handled not null, collect waypoints from mouse position
+        if (_handledObject != null)
+        {
+            Vector3 waypoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+            Vector3 waypointWorld = Camera.main.ScreenToWorldPoint(waypoint);
+            waypointWorld.z = 0.0F;
+            _handledObject.GetComponent<Battalion>().Waypoints.Add(waypointWorld + _deltaOfClickFromObject);
+        }
+
+        // mouse release
+        if (Input.GetMouseButtonUp(0))
+        {
+            _handledObject = null;
+        }
+	}
+    // =============================================================================================================== //
+    private GameObject findClosestObjectToMousePosition()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mouseWorldPosition.z = 0.0F;
+
+        GameObject closestObject = null;
+        float closestDistance = 100000.0F;
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Battalion"))
+        {
+            if (obj.GetComponent<Battalion>().Aligment != ForceAligment.FRIENDLY)
+                continue;
+
+            float distance = Vector3.Distance(obj.transform.position, mouseWorldPosition);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestObject = obj;
+                _deltaOfClickFromObject = obj.transform.position - mouseWorldPosition;
+            }
+        }
+
+        if (closestDistance <= MIN_DISTANCE_TO_MANAGE_OBJECT)
+            return closestObject;
+
+        return null;
+    }
+    // =============================================================================================================== //
+}
